44984d5a4f1d294dbfac2572ac209525f3ad10ab
MayChavez <157537170+MayChavez@users.noreply.github.com>
2024-06-22 11:36:44 -0600

    Chore: added Base attributes for Github pages on vite.config

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig(
     {
        
         appType: 'mpa',
+        base: "/McPortafolio/",
         build: {
             rollupOptions: {
                 input: Object.fromEntries(
